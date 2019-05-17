5df08fc9ce317ff5853494325cfd732902afab6e
Miika Kankare <miika@ocllo.fi>
2019-05-17 19:36:23 +0300

    Some Shellcheck and style fixes. Use Bash's [[, make Donor status check a wildcard match. Check for mysqld pid rather than bombarding systemctl is-active

--- scripts/clusterchk.sh
+++ scripts/clusterchk.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1117
 
 # Author:: Matteo Dessalvi
 #
@@ -67,11 +69,11 @@ done
 
 shift $((OPTIND-1))
 
-[ "${1:-}" = "--" ] && shift
+[[ "${1:-}" = "--" ]] && shift
 
-if [ -n "${USER}" ] && [ -n "${PASSWORD}" ]; then
+if [[ -n "${USER}" ]] && [[ -n "${PASSWORD}" ]]; then
     # Concatenate the parameters for the MySQL client:
-    USEROPTIONS="--user=${USER} --password=${PASSWORD}"
+    USEROPTIONS=("--user=${USER}" "--password=${PASSWORD}")
 else
     # Read the ini config
     read_ini_config "${INIFILE}"
@@ -81,14 +83,17 @@ fi
 # Read the config file (INI format) for MySQL:
 #
 read_ini_config () {
-  local DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-  source $DIR/read_ini.sh
+  local dir
+  dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+  # shellcheck disable=SC1090
+  . "${dir}/read_ini.sh"
 
   # Call the parser function over the debian.cnf file:
   read_ini -p IniCnf "$1"
 
   # Concatenate the parameters for the MySQL client:
-  USEROPTIONS="--user=$IniCnf__client__user --password=$IniCnf__client__password"
+  USEROPTIONS=("--user=${IniCnf__client__user:?}" "--password=${IniCnf__client__password:?}")
 }
 
 #
@@ -102,10 +107,10 @@ http_ok () {
 
   /bin/echo -ne "Content-Type: text/plain\r\n"
   /bin/echo -ne "Connection: close\r\n" 
-  /bin/echo -ne "Content-Length: $length\r\n"
+  /bin/echo -ne "Content-Length: ${length}\r\n"
 
   /bin/echo -ne "\r\n"
-  /bin/echo -ne "$message"
+  /bin/echo -ne "${message}"
   /bin/echo -ne "\r\n"
 
   sleep 0.1
@@ -122,10 +127,10 @@ http_no_access () {
 
   /bin/echo -ne "Content-Type: text/plain\r\n"
   /bin/echo -ne "Connection: close\r\n" 
-  /bin/echo -ne "Content-Length: $length\r\n"
+  /bin/echo -ne "Content-Length: ${length}\r\n"
 
   /bin/echo -ne "\r\n"
-  /bin/echo -ne "$message"
+  /bin/echo -ne "${message}"
   /bin/echo -ne "\r\n"
 
   sleep 0.1
@@ -135,17 +140,19 @@ http_no_access () {
 # Run a SQL query on the local MySQL instance.
 # 
 status_query () {
-  SQL_QUERY=`/usr/bin/mysql --host=$MYSQL_HOST --port=$MYSQL_PORT $USEROPTIONS --silent --raw -N -e "$1"`
-  RESULT=`echo $SQL_QUERY|/usr/bin/cut -d ' ' -f 2` # just remove the value label
-  echo $RESULT
+  query=$(/usr/bin/mysql --host=${MYSQL_HOST} --port=${MYSQL_PORT} "${USEROPTIONS[@]}" --silent --raw -N -e "$1")
+  
+  result=$(echo "${query}" | /usr/bin/cut -f 2) # just remove the value label
+  echo "$result"
 }
 
 #
 # Safety check: verify if MySQL is up and running.
 #
-MYSQL_STATUS=`/bin/systemctl is-active mysql.service`
-if [ "$MYSQL_STATUS" != 'active' ]; then
-    http_no_access "MySQL instance is reported $MYSQL_STATUS.\r\n"
+
+MYSQL_PID=$(pidof mysqld)
+if [[ -z "${MYSQL_PID}" ]]; then
+    http_no_access "Instance is not running.\r\n"
     exit 1
 fi
 
@@ -171,18 +178,18 @@ MYSQL_READONLY=$(status_query "SELECT @@global.read_only;")
 # Node states:
 # http://galeracluster.com/documentation-webpages/nodestates.html#node-state-changes
 #
-if [ "$GALERA_STATUS" == "Synced" ]; then
-  if [ "$MYSQL_READONLY" -eq 0 ]; then
-     http_ok "Galera status is $GALERA_STATUS\r\n"
+if [[ "${GALERA_STATUS}" == "Synced" ]]; then
+  if [[ "${MYSQL_READONLY}" -eq 0 ]]; then
+     http_ok "Status is ${GALERA_STATUS}\r\n"
   else
-     http_no_access "Galera status is $GALERA_STATUS but the local MySQL instance is reported to be read-only.\r\n"
+     http_no_access "Status is ${GALERA_STATUS}. Instance is read only.\r\n"
   fi
-elif [ "$GALERA_STATUS" == "Donor" ]; then # node is acting as 'Donor' for another node
-  if [ "$SST_METHOD" == "xtrabackup" ] || [ "$SST_METHOD" == "xtrabackup-v2" ] || [ "$SST_METHOD" == "mariabackup" ]; then
-     http_ok "Galera status is $GALERA_STATUS.\r\n" # xtrabackup is a non-blocking method
+elif [[ "${GALERA_STATUS}" == Donor* ]]; then # node is acting as 'Donor' for another node, status can be Donor/Desynced so wildcard match
+  if [[ "${SST_METHOD}" == "xtrabackup" ]] || [[ "${SST_METHOD}" == "xtrabackup-v2" ]] || [[ "${SST_METHOD}" == "mariabackup" ]]; then
+     http_ok "Status is ${GALERA_STATUS}.\r\n" # xtrabackup is a non-blocking method
   else
-     http_no_access "Galera status is $GALERA_STATUS.\r\n"
+     http_no_access "Status is ${GALERA_STATUS}.\r\n"
   fi
 else
-  http_no_access "Galera status is $GALERA_STATUS.\r\n"
+  http_no_access "Status is ${GALERA_STATUS}.\r\n"
 fi
